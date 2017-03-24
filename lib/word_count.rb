05bc645812ccb8229cc155bc942c45d13fe3e81d
JulieChristine <julie.wolsak@gmail.com>
2017-03-24 15:26:49 +0800

    added and passed test to remove punctuation in sentence

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,11 +2,14 @@ require ('pry')
 
 class String
   define_method(:wordcount) do |doubles|
+    sentence = self
+    # Remove the punctuation from the sentence
+    new_sentence = sentence.gsub(/[^0-9A-Za-z' ']/, '')
+
+    # The sentence is downcase.
     # Split by spaces the sentence in an array.
-
-    words = self.downcase().split(" ")
-
-    # method counts the number of times an argument is present in the array.
+    words = new_sentence.downcase().split(" ")
+    # Method counts the number of times an argument is present in the array.
     count = words.count(doubles)
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,10 +3,14 @@ require('word_count')
 
 describe('String#wordcount') do
   it("returns the number of times the word peck is detected in the sentence") do
-      expect("If Peter Piper picked a peck of pickled peppers, where’s the peck of pickled peppers Peter Piper picked? ".wordcount("peck")).to(eq(2))
+      expect("If Peter Piper picked a peck of pickled peppers, where’s the peck of pickled peppers Peter Piper picked?".wordcount("peck")).to(eq(2))
   end
 
-  it("returns the number of times the word hello is detected in the sentence regardless of capital letters and ") do
+  it("returns the number of times the word hello is detected in the sentence regardless of capital letters") do
       expect("Clean clams crammed in clean cans.".wordcount("clean")).to(eq(2))
   end
+
+    it("returns the number of times the word hello is detected in the sentence regardless of punctuation") do
+        expect("Can you can a can as a canner can can a can?".wordcount("can")).to(eq(6))
+    end
 end
