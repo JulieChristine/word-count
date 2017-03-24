7364e7631cad139b8da21c2c40321c7cf55bf22a
JulieChristine <julie.wolsak@gmail.com>
2017-03-24 14:38:41 +0800

    added a test that checks for counts while using capitalization

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -3,7 +3,9 @@ require ('pry')
 class String
   define_method(:wordcount) do |doubles|
     # Split by spaces the sentence in an array.
+
     words = self.downcase().split(" ")
+
     # method counts the number of times an argument is present in the array.
     count = words.count(doubles)
   end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('word_count')
 
 describe('String#wordcount') do
-  it("returns the number of peck") do
+  it("returns the number of times the word peck is detected in the sentence") do
       expect("If Peter Piper picked a peck of pickled peppers, where’s the peck of pickled peppers Peter Piper picked? ".wordcount("peck")).to(eq(2))
+  end
+
+  it("returns the number of times the word hello is detected in the sentence regardless of capital letters and ") do
+      expect("Clean clams crammed in clean cans.".wordcount("clean")).to(eq(2))
   end
 end
